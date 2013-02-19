542a18179bd63923fe71734128b14c8815b98b73
fabien <fabien@jarvis>
2013-02-19 23:28:09 +0100

    remove useless imports

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,5 @@
-from django.contrib import admin
-from django_markdown.admin import MarkdownModelAdmin
-from django_markdown.widgets import MarkdownWidget
 from blog.models import Post
-from django.db import models
-from markupfield.fields import MarkupField
+from django.contrib import admin
 
 admin.site.register(Post)
 
