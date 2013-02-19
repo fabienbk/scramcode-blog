df2ac8420564288e6d8edabeb3319f11fbf84a8b
fabien <fabien@jarvis>
2013-02-19 23:10:26 +0100

    change wsgi settings module name

--- blogsite/wsgi.py
+++ blogsite/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogsite.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
