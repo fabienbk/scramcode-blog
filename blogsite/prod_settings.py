0afbc7be426caea76fbf76874ffa222c3aeeb5db
fabien <fabien@jarvis>
2013-02-19 23:58:49 +0100

    static change

--- blogsite/prod_settings.py
+++ blogsite/prod_settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/fbenoit/webapps/scramcode_blog_static/'
+STATIC_ROOT = '/home/fbenoit/webapps/static_scramcode_blog/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
