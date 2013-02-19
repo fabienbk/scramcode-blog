a2f7cb86f1fb13ae29a2be156abc1dec4dd37b33
fabien <fabien@jarvis>
2013-02-19 23:44:51 +0100

    production static files settings

--- blogsite/prod_settings.py
+++ blogsite/prod_settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/fbenoit/webapps/scramcode_blog_static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://blog.scramcode.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
