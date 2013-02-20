ed4e4c5ec93fe726a149273a698624a1d847010f
fabien <fabien@jarvis>
2013-02-20 22:23:22 +0100

    switch to postgres for production

--- blogsite/prod_settings.py
+++ blogsite/prod_settings.py
@@ -18,12 +18,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': HOME_DIRECTORY + 'blog.db',     # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'postgresql_psycopg2',
+        'NAME': 'scramblog',
+        'USER': 'scramblog_user',                     
+        'PASSWORD': 'Ej0hD0Pzrr0k',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
